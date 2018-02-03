1e51dff16a68d2cb2b92591ae320cf6b5fa95a6e
jesse <jbarto@gmail.com>
2018-02-03 08:58:55 -0800

    working js

--- crawl.js
+++ crawl.js
@@ -1,37 +1,30 @@
-// Variable to hold request
-var request;
+var firstname = localStorage.getItem('firstname');
+var email = ('value', localStorage.getItem('email'));
+$('[name=email]').val(email);
+$('[name=firstname]').val(firstname);
 
 // on checkin submit data and collapse
 $('form').submit(function(event){
-  //document.getElementById('first name').value = localStorage.getItem('first name');
-  //document.getElementById('email').value = localStorage.getItem('email');
-    // setup some local variables
-    var $form = $(this);
-    var venue = $(this).data('id');
-    console.log(venue)
-    console.log($form)
 
-    // Serialize the data in the form
-    var serializedData = $(this).serialize();
-
+    //var serializedData = $(this).serialize();
+    var dataArray = $(this).serializeArray();
+    var venue = dataArray[2].value;
+    
     // send data to google sheet
-    request = $.ajax({
+    jQuery.ajax({
         url: "https://script.google.com/macros/s/AKfycbwxq10krHEc4Fu-zq2e1Wp-5PhIzoBHbO6HLogEcgKjbn_jkDWZ/exec",
         type: "post",
-        data: serializedData
+        data: dataArray,
+        success: function(data){
+            console.log("it worked!");
+            $('#' + venue + "c").addClass('showcheck')
+            $('#' + venue + "b").addClass('checkedin')
+        },
+        error: function(data) {
+            alert("Error");
+        }
     });
 
-    // Callback handler that will be called on success
-    request.done(function (response, textStatus, jqXHR){
-        $('#' + venue).show();
-        $('#' + venue).hide();
-        // Log a message to the console
-        console.log("it worked!");
-        console.log(response);
-        console.log(textStatus);
-        console.log(jqXHR);
-    });
-
-       // Prevent default posting of form
+    // Prevent default posting of form
     event.preventDefault();
 });
